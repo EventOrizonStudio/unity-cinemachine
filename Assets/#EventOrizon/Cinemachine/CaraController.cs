19665c996cd6dfb5e9777f0ee9785df2258595cb
alto971 <gwadaldesign@gmail.com>
2020-11-01 16:48:40 +0100

    first commit

---
+++ Assets/#EventOrizon/Cinemachine/CaraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaraController : MonoBehaviour {
+    [SerializeField]
+    private float speed = 10; //vitesse de déplacement
+
+    [SerializeField]
+    private Vector3 rotVector = new Vector3 (0, 20, 0); //vitesse de rotation
+
+    [SerializeField]
+    private Animator animator;
+
+    void Update () {
+        if (Input.GetMouseButtonDown (2)) {
+            Debug.Log ("Pressed center click: attack");
+            animator.SetTrigger ("GetHit");
+        }
+        if (Input.GetMouseButtonDown (1)) {
+            Debug.Log ("Pressed right click: attack");
+            animator.SetTrigger ("Attack");
+        }
+    }
+
+    void FixedUpdate () {
+
+        float rot = Input.GetAxis ("Horizontal"); // rot
+        float move = Input.GetAxis ("Vertical"); //move
+        //animator.SetFloat ("move", move);
+
+        animator.SetBool ("IsMoving", move != 0f ? true:false);
+        GetComponent<Rigidbody> ().velocity = transform.forward * move * speed;
+        this.transform.Rotate (rotVector * rot);
+    }
+
+    public void DealDamage(){
+        Debug.Log ("DealDamage");
+    }
+
+    public void FireProjectile(){
+        Debug.Log ("FireProjectile");
+    }
+}
---
+++ Assets/#EventOrizon/Cinemachine/SwitchView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SwitchView : MonoBehaviour
+{
+    [SerializeField]private CinemachineVirtualCamera cineCam;
+    [SerializeField]private  List<Transform> targets;
+
+    public int currentInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cineCam = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown (KeyCode.Space)) {
+            Debug.Log ("switch target");
+            currentInt++;
+            if(currentInt >= targets.Count){
+                currentInt = 0;
+            }
+            cineCam.Follow = targets[currentInt];
+
+        }
+    }
+}
